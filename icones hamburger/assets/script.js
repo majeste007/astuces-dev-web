95abbff7f68e18ac00c077b3e4f71b4834a3ade1
steph <stephmajeste@gmail.com>
2024-08-15 22:22:09 +0200

    Astuces côté developpement web maintenant

---
+++ icones hamburger/assets/script.js
@@ -0,0 +1,8 @@
+// <!-- menu hamburger -->
+const allBoxes = document.querySelectorAll(".box");
+
+allBoxes.forEach(box =>{
+    box.addEventListener("click",e =>{
+        e.target.classList.toggle('active');
+    })
+})
